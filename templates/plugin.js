7d4cf2145f0b28d21e9701fbfcfd977cecb2299c
Touch <chantouchsek.cs83@gmail.com>
2025-12-04 15:35:46 +0700

    refactor!: improve request key generation and update dependencies

--- templates/plugin.js
+++ templates/plugin.js
@@ -2,22 +2,16 @@ import axios from 'axios'
 import { isPlainObject } from 'is-plain-object'
 
 function createRequestKey(url, params) {
-  let requestKey = url;
-  if (params) {
-    requestKey += `:${createStringFromParameters(params)}`
-  }
-  return requestKey;
+  return params ? `${url}:${createStringFromParameters(params)}` : url;
 }
 
 function createStringFromParameters(obj) {
-  const keysArr = [];
+  const parts = [];
   for (const key in obj) {
-    keysArr.push(key);
-    if (isPlainObject(obj[key])) {
-      keysArr.push(createStringFromParameters(obj[key]));
-    }
+    parts.push(key);
+    if (isPlainObject(obj[key])) parts.push(createStringFromParameters(obj[key]));
   }
-  return keysArr.join('|');
+  return parts.join('|');
 }
 
 function createCancelMessage(requestKey, paramsStr) {
@@ -30,9 +24,7 @@ function createCancelMessage(requestKey, paramsStr) {
 }
 
 export default function ({ $axios, app }) {
-
   $axios.activeRequests = {};
-
   $axios.onRequest((config) => {
     let blockerConfigContainer = config
     <% if (options.headerBlockerKey) { %>
@@ -46,41 +38,29 @@ export default function ({ $axios, app }) {
     if (requestBlockingAllowed === undefined) {
       requestBlockingAllowed = <%= options.blockByDefault %>;
     }
-    if (!requestBlockingAllowed) {
-      return config;
-    }
+    if (!requestBlockingAllowed) return config;
 
-    //Check if user has set a custom requestKey
     let { requestKey } = blockerConfigContainer;
-    if (!requestKey) {
-      //If there is no custom requestKey, create a default requestKey based on url and parameters
-      requestKey = createRequestKey(config.baseURL + config.url, config.params);
-    }
+    if (!requestKey) requestKey = createRequestKey(config.baseURL + config.url, config.params);
     const paramsStr = JSON.stringify(config.params);
-    //If another request with the same requestKey already exists, cancel it
     if ($axios.activeRequests.hasOwnProperty(requestKey) && $axios.activeRequests[requestKey].cancelToken) {
       $axios.activeRequests[requestKey].cancelToken.cancel(createCancelMessage(requestKey, paramsStr));
     }
-    if (!$axios.hasOwnProperty(requestKey)) {
-      //If request has not been sent before, create a custom promise
+    if (!$axios.activeRequests.hasOwnProperty(requestKey)) {
       let reqResolve, reqReject;
       const promise = new Promise((resolve, reject) => {
         reqResolve = resolve;
         reqReject = reject;
       });
-      //Insert current request to activeRequests
       $axios.activeRequests[requestKey] = {
         promise: promise,
         resolve: reqResolve,
         reject: reqReject
       }
     }
-    //Update the active request's params
     $axios.activeRequests[requestKey].paramsStr = paramsStr;
-    //Create a cancel token for current request
     const cancelToken = axios.CancelToken.source();
     $axios.activeRequests[requestKey].cancelToken = cancelToken;
-    //Add the cancel token to the request
     return {
       ...config,
       cancelToken: cancelToken && cancelToken.token
@@ -88,17 +68,12 @@ export default function ({ $axios, app }) {
   });
 
   $axios.onError((err) => {
-    //Check if error message has a requestKey set in active requests
     if (err.hasOwnProperty('message') && err.message.hasOwnProperty('requestKey') && $axios.activeRequests.hasOwnProperty(err.message.requestKey)) {
       const currentRequest = $axios.activeRequests[err.message.requestKey];
-      //Check if error concerns a cancellation
       if (err.message && err.message.statusCode === 100 && currentRequest && currentRequest.paramsStr === err.message.paramsStr) {
-        //Handle the cancellation error
         <% if (options.debug) { %>
-          // eslint-disable-next-line no-console
           console.warn(err.message.message);
         <% } %>
-        //Return a promise to the active request that overrides the current one
         return $axios.activeRequests[err.message.requestKey].promise;
       }
     }
@@ -106,14 +81,9 @@ export default function ({ $axios, app }) {
   });
 
   $axios.onResponse((response) => {
-    //Check if user has set a custom requestKey
     let { requestKey } = response.config;
-    if (!requestKey) {
-      //If there is no custom requestKey, create a default requestKey based on url and parameters
-      requestKey = createRequestKey(response.config.url, response.config.params);
-    }
+    if (!requestKey) requestKey = createRequestKey(response.config.baseURL + response.config.url, response.config.params);
     if ($axios.activeRequests.hasOwnProperty(requestKey)) {
-      //Inform all previously cancelled requests with the current response & remove requestKey from activeRequests
       $axios.activeRequests[requestKey].resolve(response);
       delete $axios.activeRequests[requestKey];
     }
